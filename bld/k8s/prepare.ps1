85b85259368b94b7eec8da40933c2a05e78772f5
Jesper Balle <Jesper.Balle@valtech.com>
2022-03-17 12:15:20 +0100

    fix kustomize

--- bld/k8s/prepare.ps1
+++ bld/k8s/prepare.ps1
@@ -77,6 +77,30 @@ function RemoveBases {
     Set-Content -Path "${Path}" -Value $content -Encoding UTF8
 }
 
+function GenerateKustomizationFileForFolder {
+    param(
+        $Path
+    )
+
+    if (-not (Test-Path $Path -PathType Container)) {
+        Write-Error "Cannot create Kustomization file as ${Path} is not folder"
+        return
+    }
+
+    $resultFile = Join-Path $Path "kustomization.yaml"
+    If (Test-Path $resultFile) {
+        Write-Warning "Kustomization file $resultFile already exists"
+        return        
+    }
+
+    $files = Get-ChildItem -Path $Path -Filter "*.yaml"
+    Write-Host ("Create {0} for {1} files" -f $resultFile, $files.Length) -ForegroundColor Green
+    $content = "apiVersion: kustomize.config.k8s.io/v1beta1`nkind: Kustomization`n`nresources:`n"
+    $files | ForEach-Object { $content += ("- {0}`n" -f $_.Name) }
+    $content | Out-File -FilePath $resultFile -Encoding utf8 -Force
+}
+
+
 $sitecoreDestination = "${DestinationRoot}/${Platform}/${Topology}"
 
 if (-not $SkipDownload) {
@@ -97,6 +121,7 @@ if (-not $SkipCorrections) {
 
     RemoveBases -Path (Resolve-path $sitecoreDestination\kustomization.yaml)
     RemoveTlsSecretEntries -Path (Resolve-Path $sitecoreDestination\secrets\kustomization.yaml)
+    GenerateKustomizationFileForFolder -Path (Resolve-Path ${sitecoreDestination}\volumes\azurefile)
 }
 
 Write-Host "Done!" -ForegroundColor Green
