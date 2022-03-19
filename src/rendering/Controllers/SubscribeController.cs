713164d62d26b975b167ad64b64b7293fae185a5
Jesper Balle <Jesper.Balle@valtech.com>
2022-03-19 15:36:14 +0100

    Add missing model and controller

---
+++ src/rendering/Controllers/SubscribeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SugCon.SitecoreSend.Models;
+using SugCon.SitecoreSend.Services;
+using System.Threading.Tasks;
+
+namespace SugCon.SitecoreSend.Controllers
+{
+    [ApiController]
+    public class SubscribeController : Controller
+    {
+        private readonly ISendService _service;
+
+        public SubscribeController(ISendService service)
+        {
+            _service = service;
+        }
+
+        [Route("/subscribe/{listId}"), HttpPost]
+        public async Task<IActionResult> Index(string listId, MooSendSubscriber subscriber)
+        {
+            await _service.Subscribe(listId, subscriber);
+            return Redirect("/?subscribe=ok");
+        }
+    }
+
+    public class SubscribeModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/rendering/Models/SubscribeFormModel.cs
@@ -0,0 +1,11 @@
+﻿using Sitecore.AspNet.RenderingEngine.Binding.Attributes;
+
+namespace SugCon.SitecoreSend
+{
+    public class SubscribeFormModel
+    {
+        [SitecoreComponentField(Name = "List id")]
+        public string ListId { get; set; }
+        // public ICollection<MooSendCustomField> Fields { get; internal set; }
+    }
+}
