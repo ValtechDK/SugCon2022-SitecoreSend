6202380c6bee042d037c542de820458ed59d6848
Jesper Balle <Jesper.Balle@valtech.com>
2022-03-23 19:17:35 +0100

    add webhook

---
+++ src/rendering/Controllers/WebHookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace SugCon.SitecoreSend.Controllers
+{
+    public class WebHookController : Controller
+    {
+        private readonly ILogger<WebHookController> _logger;
+
+        public WebHookController(ILogger<WebHookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("/api/webhooks/incoming/moosend"), HttpPost]
+        public ContentResult WebHook([FromBody] object obj)
+        {
+            _logger.LogInformation($"Received webhook: \r\n{JsonConvert.SerializeObject(obj, Formatting.Indented)}");
+            return Content("OK");
+        }
+    }
+}
